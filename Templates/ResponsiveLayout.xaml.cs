583104c7f60d3c4f6fd93a92d475efcca8ff3a9a
rtownsend9 <143042062+rtownsend9@users.noreply.github.com>
2024-07-16 12:48:36 -0500

    Added responsive app layout

---
+++ Templates/ResponsiveLayout.xaml.cs
@@ -0,0 +1,7 @@
+﻿private void ResponsiveLayout(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+{
+    if (e.Size.Width > 640)
+        VisualStateManager.GoToState(this, "WideState", false);
+    else
+        VisualStateManager.GoToState(this, "DefaultState", false);
+}
