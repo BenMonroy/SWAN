ccfe662221157f3b818626c5b4c3f67178e3c280
root <root@DESKTOP-G9SNP1K.localdomain>
2024-07-16 14:57:25 -0400

    trying to resolve merge conflicts

---
+++ Templates/HierarchialData.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace SWAN
+{
+    public partial class HierarchialData : UserControl
+    {
+        public HierarchialData()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var parent1 = new Parent("Parent #1")
+            {
+                ChildItems = {
+                    new Child("Child Item #1.1"),
+                    new Child("Child Item #1.2"),
+                    new Child("Child Item #1.3")
+                }
+            };
+
+            var parent2 = new Parent("Parent #2")
+            {
+                ChildItems = {
+                    new Child("Child Item #2.1"),
+                    new Child("Child Item #2.2"),
+                    new Child("Child Item #2.3")
+                }
+            };
+
+            List<Parent> parents = new List<Parent> { parent1, parent2 };
+            treeView.ItemsSource = parents;
+        }
+    }
+
+    public class Child
+    {
+        public Child(string title)
+        {
+            Title = title;
+        }
+        public string Title { get; set; }
+    }
+
+    public class Parent
+    {
+        public Parent(string title)
+        {
+            Title = title;
+            ChildItems = new List<Child>();
+        }
+        public string Title { get; set; }
+        public List<Child> ChildItems { get; set; }
+    }
+}
---
+++ Templates/ResponsiveLayout.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows; // Ensure using WPF namespaces
+using System.Windows.Controls;
+
+namespace SWAN
+{
+    public partial class ResponsiveLayout : Page
+    {
+        public ResponsiveLayout()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Handle size changes if needed
+        }
+    }
+}
