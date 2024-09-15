3441f4895a9ffb19bd93baae6c801531655d8c6c
BenMonroy <ben.a.monroy@gmail.com>
2024-09-15 12:17:34 -0400

    moved severity to conceptual control instead of individual for physical controls

--- Components/ExpandableCheckBox.xaml.cs
+++ Components/ExpandableCheckBox.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +26,77 @@ namespace SWAN.Components
         {
             InitializeComponent();
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // Cast sender as CheckBox
+            var checkBox = sender as CheckBox;
+
+            // Get the DataContext of the CheckBox, which is bound to PhysicalControl
+            var dataContext = checkBox.DataContext as PhysicalControl;
+
+            if (dataContext != null)
+            {
+                // Set the Passed property of the bound PhysicalControl to true
+                dataContext.Passed = true;
+
+                // Get the parent ConceptualCheckBox by traversing the visual tree
+                var parentExpander = FindVisualParent<Expander>(checkBox);
+                if (parentExpander == null) { MessageBox.Show("Parent control not found"); }
+                if (parentExpander != null)
+                {
+                    var parentDataContext = parentExpander.DataContext;
+                    var conceptualCheckBox = parentExpander.DataContext as ConceptualCheckBox;
+                    if (conceptualCheckBox == null) { MessageBox.Show("Conceptual Checkbox not found"); }
+                    if (conceptualCheckBox != null)
+                    {
+                        // Check if all PhysicalControl items are true
+                        conceptualCheckBox.AllPassed = conceptualCheckBox.PhysicalControls.All(pc => pc.Passed);
+                        //this is for testing, change this to be a style 
+                       // if (conceptualCheckBox.AllPassed) {parentExpander.Background = Brushes.LightGreen; }
+                       // if (!conceptualCheckBox.AllPassed) { parentExpander.Background = Brushes.Blue; }
+                    }
+                }
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // Cast sender as CheckBox
+            var checkBox = sender as CheckBox;
+
+            // Get the DataContext of the CheckBox, which is bound to PhysicalControl
+            var dataContext = checkBox.DataContext as PhysicalControl;
+
+            if (dataContext != null)
+            {
+                // Set the Passed property of the bound PhysicalControl to false
+                dataContext.Passed = false;
+
+                // Get the parent ConceptualCheckBox by traversing the visual tree
+                var parentExpander = FindVisualParent<Expander>(checkBox);
+                if (parentExpander != null)
+                {
+                    var conceptualCheckBox = parentExpander.DataContext as ConceptualCheckBox;
+                    if (conceptualCheckBox != null)
+                    {
+                        conceptualCheckBox.AllPassed = false;
+                    }
+                    //for testing remove this and switch to a style
+                  //  if (!conceptualCheckBox.AllPassed) { parentExpander.Background = Brushes.Blue; };
+                }
+            }
+        }
+
+        
+        private T FindVisualParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            while (child != null && !(child is T))
+            {
+                child = VisualTreeHelper.GetParent(child);
+            }
+            return child as T;
+        }
+
     }
 }
