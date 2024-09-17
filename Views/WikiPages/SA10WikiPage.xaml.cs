e979c7c080271aeedba22ac06167b3648c2b9288
Jake Mayer <jakemay3r@gmail.com>
2024-09-17 02:05:51 -0400

    Completed SubNavigation for all existing wiki pages.
    TODO: Refactor using dictionary as code has gotten out of hand.

--- Views/WikiPages/SA10WikiPage.xaml.cs
+++ Views/WikiPages/SA10WikiPage.xaml.cs
@@ -6,66 +6,101 @@ using System.Windows.Media;
 
 namespace SWAN.Views.WikiPages
 {
-
-
     public partial class SA10WikiPage : Page
     {
         public SA10WikiPage()
         {
             InitializeComponent();
 
-            //// Delay scrolling until after the UI is fully rendered
-            //Dispatcher.BeginInvoke(new Action(() =>
-            //{
-            //    ScrollToSubsection();
-            //}), DispatcherPriority.Loaded);
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                ScrollToSubsection();
+            }), DispatcherPriority.Loaded);
         }
 
-        //    private void ScrollToSubsection()
-        //    {
-        //        string subsectionName = Application.Current.Properties["SubsectionName"] as string;
-        //        if (!string.IsNullOrEmpty(subsectionName))
-        //        {
-        //            // Find the target paragraph
-        //            Paragraph targetParagraph = FindParagraphByTag(flowDocument.Blocks, subsectionName);
-        //            if (targetParagraph != null)
-        //            {
-        //                // Ensure layout is updated
-        //                flowDocumentScrollViewer.UpdateLayout();
+        private void ScrollToSubsection()
+        {
+            string subsectionName = Application.Current.Properties["SubsectionName"] as string;
+            if (!string.IsNullOrEmpty(subsectionName))
+            {
+                Paragraph targetParagraph = FindParagraphByTag(flowDocument.Blocks, subsectionName);
+                if (targetParagraph != null)
+                {
+                    // Get the vertical offset of the paragraph
+                    double verticalOffset = GetParagraphVerticalOffset(targetParagraph);
 
-        //                // Bring the paragraph into view
-        //                targetParagraph.BringIntoView();
-        //            }
-        //            else
-        //            {
-        //                MessageBox.Show($"Subsection '{subsectionName}' not found in the document.");
-        //            }
-        //        }
-        //    }
+                    // Access the internal ScrollViewer
+                    ScrollViewer scrollViewer = GetScrollViewer(flowDocumentScrollViewer);
+                    if (scrollViewer != null)
+                    {
+                        // Scroll to the exact vertical offset
+                        scrollViewer.ScrollToVerticalOffset(verticalOffset);
+                    }
+                    else
+                    {
+                        MessageBox.Show("ScrollViewer not found.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Subsection '{subsectionName}' not found in the document.");
+                }
+                Application.Current.Properties.Remove("SubsectionName");
 
-        //    private Paragraph FindParagraphByTag(BlockCollection blocks, string tag)
-        //    {
-        //        foreach (Block block in blocks)
-        //        {
-        //            if (block is Paragraph paragraph)
-        //            {
-        //                if ((string)paragraph.Tag == tag)
-        //                {
-        //                    return paragraph;
-        //                }
-        //            }
-        //            else if (block is Section section)
-        //            {
-        //                Paragraph result = FindParagraphByTag(section.Blocks, tag);
-        //                if (result != null)
-        //                {
-        //                    return result;
-        //                }
-        //            }
-        //        }
-        //        return null;
-        //    }
+            }
+        }
+
+        private double GetParagraphVerticalOffset(Paragraph targetParagraph)
+        {
+            // Force the layout to update so we can get accurate positions
+            flowDocumentScrollViewer.UpdateLayout();
+
+            // Get the TextPointer at the start of the paragraph
+            TextPointer contentStart = targetParagraph.ContentStart;
+
+            // Get a bounding rectangle for the TextPointer
+            Rect rect = contentStart.GetCharacterRect(LogicalDirection.Forward);
+
+            // The vertical offset is the Y-coordinate of the rectangle
+            return rect.Top;
+        }
+
+        private ScrollViewer GetScrollViewer(DependencyObject depObj)
+        {
+            if (depObj is ScrollViewer)
+                return (ScrollViewer)depObj;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                ScrollViewer result = GetScrollViewer(child);
+                if (result != null)
+                    return result;
+            }
+            return null;
+        }
+
+        private Paragraph FindParagraphByTag(BlockCollection blocks, string tag)
+        {
+            foreach (Block block in blocks)
+            {
+                if (block is Paragraph paragraph)
+                {
+                    if ((string)paragraph.Tag == tag)
+                    {
+                        return paragraph;
+                    }
+                }
+                else if (block is Section section)
+                {
+                    Paragraph result = FindParagraphByTag(section.Blocks, tag);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
     }
-
-
 }
--- Views/WikiPages/SA3WikiPage.xaml.cs
+++ Views/WikiPages/SA3WikiPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SWAN.Views.WikiPages
 {
@@ -23,6 +24,96 @@ namespace SWAN.Views.WikiPages
         public SA3WikiPage()
         {
             InitializeComponent();
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                ScrollToSubsection();
+            }), DispatcherPriority.Loaded);
+        }
+
+        private void ScrollToSubsection()
+        {
+            string subsectionName = Application.Current.Properties["SubsectionName"] as string;
+            if (!string.IsNullOrEmpty(subsectionName))
+            {
+                Paragraph targetParagraph = FindParagraphByTag(flowDocument.Blocks, subsectionName);
+                if (targetParagraph != null)
+                {
+                    // Get the vertical offset of the paragraph
+                    double verticalOffset = GetParagraphVerticalOffset(targetParagraph);
+
+                    // Access the internal ScrollViewer
+                    ScrollViewer scrollViewer = GetScrollViewer(flowDocumentScrollViewer);
+                    if (scrollViewer != null)
+                    {
+                        // Scroll to the exact vertical offset
+                        scrollViewer.ScrollToVerticalOffset(verticalOffset);
+                    }
+                    else
+                    {
+                        MessageBox.Show("ScrollViewer not found.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Subsection '{subsectionName}' not found in the document.");
+                }
+                Application.Current.Properties.Remove("SubsectionName");
+
+            }
+        }
+
+        private double GetParagraphVerticalOffset(Paragraph targetParagraph)
+        {
+            // Force the layout to update so we can get accurate positions
+            flowDocumentScrollViewer.UpdateLayout();
+
+            // Get the TextPointer at the start of the paragraph
+            TextPointer contentStart = targetParagraph.ContentStart;
+
+            // Get a bounding rectangle for the TextPointer
+            Rect rect = contentStart.GetCharacterRect(LogicalDirection.Forward);
+
+            // The vertical offset is the Y-coordinate of the rectangle
+            return rect.Top;
+        }
+
+        private ScrollViewer GetScrollViewer(DependencyObject depObj)
+        {
+            if (depObj is ScrollViewer)
+                return (ScrollViewer)depObj;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                ScrollViewer result = GetScrollViewer(child);
+                if (result != null)
+                    return result;
+            }
+            return null;
+        }
+
+        private Paragraph FindParagraphByTag(BlockCollection blocks, string tag)
+        {
+            foreach (Block block in blocks)
+            {
+                if (block is Paragraph paragraph)
+                {
+                    if ((string)paragraph.Tag == tag)
+                    {
+                        return paragraph;
+                    }
+                }
+                else if (block is Section section)
+                {
+                    Paragraph result = FindParagraphByTag(section.Blocks, tag);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
